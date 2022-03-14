39b3440928e262a85cc7790a87a2d90c6cdeb468
hasnatulhaq <hasnatulhaq@outlook.com>
2022-03-14 12:43:23 +0500

    updating

---
+++ src/features/box/Box.js
@@ -0,0 +1,10 @@
+import './Box.css';
+export function Box(){
+     return(
+         <>
+           <div className="box">
+               <h1>hello world</h1>
+           </div>
+         </>
+     )
+}
